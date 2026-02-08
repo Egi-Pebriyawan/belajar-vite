fcd0cd38a07f39c0ed07063085f5ab08a122ce86
Egi Pebriyawan <egipebriyawan02@gmail.com>
2026-02-08 18:27:19 +0700

    feat: inisialisasi proyek Vite — tambah source, package.json, package-lock.json, dan dist

---
+++ say-hello.js
@@ -0,0 +1,19 @@
+// 3. Named Function Expression
+export const sayHello = function sayHello(name) {
+  console.log(`Hello, ${name}!`);
+};
+
+// // 1. Function Declaration
+// export function sayHello(name) {
+//   console.log(`Hello, ${name}!`);
+// }
+
+// // 2. Function Expression
+// export const sayHello = function (name) {
+//   console.log(`Hello, ${name}!`);
+// };
+
+// 4. Arrow Function (yang sudah Anda gunakan)
+// export const sayHello = (name) => {
+//     console.log(`Hello, ${name}!`);
+// };
